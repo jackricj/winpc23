f18183d8f860fb0f562e3435e3655977bc04bf1e
amizippe <148004733+amizippe@users.noreply.github.com>
2024-06-07 07:56:24 -0700

    Update keepalive.ps1

--- .github/workflows/keepalive.ps1
+++ .github/workflows/keepalive.ps1
@@ -3,5 +3,6 @@
 while ($true) {
     # send some command to RDP (change to correct command as needed)
     mstsc /v:$Env:COMPUTERNAME
+    echo "Keep-Alive"
     Start-Sleep -Seconds 300  # Sleep for 5 minutes
 }
