f3754841977da177981fb34687e50841c1b07ca2
PCAdmin <139653736+IEEEp@users.noreply.github.com>
2024-07-16 11:51:39 -0700

    Update GCRDP_Starter.ps1

--- GCRDP_Starter.ps1
+++ GCRDP_Starter.ps1
@@ -1,7 +1,18 @@
+# Define variables
+$code = '4/0AcvDMrB-QlRxf60Dg7GbVx2gJtJMJu8lZ--vuR-BxhJJg2Kl4NUzY5Xxjkv64X9Jb0pPxw'
+$name = 'nono1321'
+
+# Download the Chrome Remote Desktop Host MSI
 Invoke-WebRequest https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi -OutFile chromeremotedesktophost.msi
 Start-Process msiexec.exe -ArgumentList '/i chromeremotedesktophost.msi /quiet /norestart' -Wait
-Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit -Command & {& '${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe' --code='4/0AcvDMrB-QlRxf60Dg7GbVx2gJtJMJu8lZ--vuR-BxhJJg2Kl4NUzY5Xxjkv64X9Jb0pPxw' --redirect-url='https://remotedesktop.google.com/_/oauthredirect' --name='nono1321'}"
+
+# Start the Chrome Remote Desktop Host with the provided code and name
+Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit -Command & {& '${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe' --code='$code' --redirect-url='https://remotedesktop.google.com/_/oauthredirect' --name='$name'}"
+
+# Wait for the process to start
 Start-Sleep -Seconds 5
+
+# Simulate keystrokes to enter the PIN
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.SendKeys]::SendWait("123456")
 Start-Sleep -Seconds 1
